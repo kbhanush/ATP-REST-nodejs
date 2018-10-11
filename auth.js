034244890a724aaccfa5675828c3a6ed7f1d57a6
Danny <danny@Dannys-MacBook-Pro.local>
2018-10-11 16:29:51 -0400

    auto pull config file

--- auth.js
+++ auth.js
@@ -13,16 +13,21 @@ var os = require('os');
 
 /* Begin ---- Tenant auth info */
 
-var tenancyId=  "ocid1.tenancy.oc1..aaaaaaaawrgt5au6hbledhhyas2secm3q2atqiuvihck45rbi3jyc5tfyfga";
-var authUserId= "ocid1.user.oc1..aaaaaaaa4tfljmeejbutmrydmms62ooqdl2ay4x52m2xutb35nylnn6vupja";
-var keyFingerprint = "d3:5b:36:21:2e:82:4b:b2:cf:de:31:4f:df:f2:ba:ad";
+
+var data = fs.readFileSync("/root/.oci/config", 'utf8');
+var lines = data.split("\n");
+
+var authUserId= lines[1].split("=")[1];
+var keyFingerprint= lines[2].split("=")[1];
+var tenancyId= lines[4].split("=")[1];
+
+//User will enter their compartment here
 var rootCompartment = "ocid1.tenancy.oc1..aaaaaaaawrgt5au6hbledhhyas2secm3q2atqiuvihck45rbi3jyc5tfyfga";
-var privateKeyPath = "/Users/danny/Documents/ATP1/ATP-REST-nodejs/oci_api_key.pem";
 
 // if(privateKeyPath.indexOf("~/") === 0) {
 //     privateKeyPath = privateKeyPath.replace("~", os.homedir())
 // }
-var privateKey = fs.readFileSync(privateKeyPath, 'ascii');
+var privateKey = fs.readFileSync("/root/.oci/oci_api_key.pem", 'ascii');
 
 /* End ---- Tenant auth info */
 
